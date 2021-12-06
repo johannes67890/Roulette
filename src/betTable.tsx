a3553adf835024767ed529e7a9eae2daa1ca2993
Greg <selivanov.grisha@gmail.com>
2021-12-06 09:07:04 +0100

    buttons done(?)

--- src/betTable.tsx
+++ src/betTable.tsx
@@ -10,18 +10,18 @@ const Button: React.FC<{Style?: string, onClick?: () => unknown}> = ({children, 
 function Table() {
   return (
   <div>
-    <div className="flex-auto">
+    <div className="flex gap-2 m-3">
       <Button Style={"green"}>0</Button>
-      <div className="m-6 grid grid-cols-9 grid-rows-3 gap-5">
+      <div className="grid grid-cols-9 flex-1 grid-rows-3 gap-2">
         {Tiles.slice(1).map((value, index) => <Button key={index} Style={value.color}>{value.val}</Button>)}
       </div>
     </div>
-    <div className="m-6 grid grid-cols-3 grid-rows-1 gap-5">
+    <div className="m-3 grid grid-cols-3 grid-rows-1 gap-2">
     <Button Style={"black"}>1st Twelve</Button>
     <Button Style={"black"}>2nd Twelve</Button>
     <Button Style={"black"}>3rd Twelve</Button>
     </div>
-    <div className="m-6 grid grid-cols-2 grid-rows-1 gap-5">
+    <div className="m-3 grid grid-cols-2 grid-rows-1 gap-2">
     <Button Style={"black"}>Odd</Button>
     <Button Style={"black"}>Even</Button>
     </div>

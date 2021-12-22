45e6c19f7f526d266635960a490a7b7e0cc35244
johannes67890 <johannes@orager.dk>
2021-12-22 23:25:13 +0100

    minor

--- src/index.tsx
+++ src/index.tsx
@@ -5,7 +5,6 @@ import Wheel from "./components//Wheel";
 import Winner from "./components/Winner";
 import { TileType } from "./components/Tiles";
 import History from "./components/History";
-import Test from "./components/Test";
 
 const Index = () => {
   const [result, setResult] = useState<TileType | undefined>();
@@ -17,7 +16,6 @@ const Index = () => {
         <Winner result={result} />
         <div className="flex flex-row">
           <History result={result} />
-          <Test />
         </div>
       </div>
     </React.StrictMode>
